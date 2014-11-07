892c890db09bc002bf1cf312998836ef79f02c59
tjian <tjian123@foxmail.com>
2014-11-07 19:28:58 +0800

    add test files

--- workforpython/test.py
+++ workforpython/test.py
@@ -1,6 +1,17 @@
-#! /env/python 
+#! /usr/bin/env python 
 
 print "hello"
 
 class Person():
 	pass
+
+x = input("please input x:")
+print x
+
+text = "Hello, %s, i'm %s"
+values = ('Jacky','Kimy')
+print text % values
+
+
+raw_input("any key to exit.")
+
